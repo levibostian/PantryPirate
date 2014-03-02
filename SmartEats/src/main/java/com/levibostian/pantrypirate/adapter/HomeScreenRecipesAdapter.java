f6e14554fe0aa6d337dd1f700fa54d0eb6b0128c
Levi Bostian <levi.bostian@gmail.com>
2014-03-02 10:54:41 -0600

    Quick and dirty implementation of multi-line textview word wrap.

--- SmartEats/src/main/java/com/levibostian/pantrypirate/adapter/HomeScreenRecipesAdapter.java
+++ SmartEats/src/main/java/com/levibostian/pantrypirate/adapter/HomeScreenRecipesAdapter.java
@@ -1,6 +1,7 @@
 package com.levibostian.pantrypirate.adapter;
 
 import android.app.Activity;
+import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,12 +11,15 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import com.levibostian.pantrypirate.R;
 import com.levibostian.pantrypirate.model.RecipesModel;
+import org.w3c.dom.Text;
 
 import java.util.Random;
 
 public class HomeScreenRecipesAdapter extends ArrayAdapter {
     private Activity mContext;
     private RecipesModel mRecipesModel;
+
+    private static final int MAX_LENGTH_RECIPE_TITLE = 45;
 
     public HomeScreenRecipesAdapter(Activity context, RecipesModel model) {
         super(context, R.layout.home_screen_recipes_list_item, model.getRecipes());
@@ -46,11 +50,19 @@ public class HomeScreenRecipesAdapter extends ArrayAdapter {
         }
 
         ViewHolder holder = (ViewHolder) rowView.getTag();
-        holder.recipeTitle.setText(mRecipesModel.getRecipeTitle(position));
+        setRecipeText(holder.recipeTitle, mRecipesModel.getRecipeTitle(position));
         holder.recipeImage.setImageBitmap(mRecipesModel.getRecipeBitmap(position));
         setRandomRating(holder.recipeRating);
 
         return rowView;
+    }
+
+    private void setRecipeText(TextView recipeTitle, String text) {
+        if (text.length() > MAX_LENGTH_RECIPE_TITLE) {
+            text = text.substring(0, MAX_LENGTH_RECIPE_TITLE) + "...";
+        }
+
+        recipeTitle.setText(text);
     }
 
     private void setRandomRating(RatingBar ratingBar) {
