be3b2d03fad9bf287f3bb86e02afd6530b1fcb98
Levi Bostian <levi.bostian@gmail.com>
2014-03-02 14:29:43 -0600

    Create dialog to prompt user for method of inserting inventory item. Add barcode scanning intent.

--- SmartEats/src/main/java/com/levibostian/pantrypirate/fragment/InventoryFragment.java
+++ SmartEats/src/main/java/com/levibostian/pantrypirate/fragment/InventoryFragment.java
@@ -1,22 +1,32 @@
 package com.levibostian.pantrypirate.fragment;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 import com.levibostian.pantrypirate.R;
 import com.levibostian.pantrypirate.adapter.InventoryListAdapter;
 import com.levibostian.pantrypirate.model.InventoryModel;
 import com.levibostian.pantrypirate.vo.FoodItem;
 import de.timroes.android.listview.EnhancedListView;
 
-public class InventoryFragment extends BaseFragment implements EnhancedListView.OnDismissCallback {
+import static com.levibostian.pantrypirate.fragment.AddInventorySelectDialogFragment.*;
+
+public class InventoryFragment extends BaseFragment implements EnhancedListView.OnDismissCallback,
+                                                               AddInventorySelectDialogFragment.AddInventoryItemSelectMethodCallback {
     private EnhancedListView mInventoryList;
     private InventoryListAdapter mAdapter;
     private RelativeLayout mPantryBareView;
+
+    private static final String ADD_ITEM_DIALOG = "addItemDialog";
+
+    private static final int BARCODE_SCAN_INTENT = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,8 +47,21 @@ public class InventoryFragment extends BaseFragment implements EnhancedListView.
         mInventoryList = (EnhancedListView) view.findViewById(R.id.inventory_list);
         mPantryBareView = (RelativeLayout) view.findViewById(R.id.pantry_bare_view);
 
+        Button addButton = (Button) view.findViewById(R.id.add_inventory_item);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                promptInventoryAddMethod();
+            }
+        });
+
         populateInventory();
         checkShowInventoryLowView();
+    }
+
+    private void promptInventoryAddMethod() {
+        DialogFragment dialog = new AddInventorySelectDialogFragment(this);
+        dialog.show(getFragmentManager(), ADD_ITEM_DIALOG);
     }
 
     private void populateInventory() {
@@ -84,5 +107,38 @@ public class InventoryFragment extends BaseFragment implements EnhancedListView.
     @Override
     public void resetFragmentContent() {
         populateInventoryList();
+    }
+
+    @Override
+    public void addInventoryItemMethodSelected(int pos) {
+        switch (pos) {
+            case SCAN_BARCODE:
+                scanBarcodeIntent();
+                break;
+            case SPEECH_TO_TEXT:
+                speechToTextIntent();
+                break;
+        }
+    }
+
+    private void scanBarcodeIntent() {
+        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+        intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
+        startActivityForResult(intent, BARCODE_SCAN_INTENT);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == BARCODE_SCAN_INTENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+            }
+        }
+    }
+
+    private void speechToTextIntent() {
+
     }
 }
